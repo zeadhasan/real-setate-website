fe1039aa6b8e01675d88ce6faf059dc369af5d34
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-09-13 23:33:57 +0600

    first commit

---
+++ src/components/common/header/Header.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Header.css";
+import logo from "../../../asset/Images/logo.png";
+import { Link } from "react-router-dom";
+import { nav } from "../../data/Data";
+
+const Header = () => {
+  const [navList, setNavList] = useState(false);
+  return (
+    <>
+      <header>
+        <div className="container flex">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="nav">
+            <ul className={navList ? "small" : "flex"}>
+              {nav.map((list, index) => (
+                <li key={index}>
+                  <Link to={list.path}>{list.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="button flex">
+            <span>Wish List</span>
+            <button className="btn1">
+              <i className="fa fa-sign-out" /> Sign In
+            </button>
+          </div>
+          <div className="toggle">
+            <button onClick={() => setNavList(!navList)}>
+              {navList ? (
+                <i className="fa fa-times" />
+              ) : (
+                <i className="fa fa-bars" />
+              )}
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/pages/Pages.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "../common/header/Header";
+import Home from "../home/Home";
+
+const Pages = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+export default Pages;
